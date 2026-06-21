828c653891effd3ab34dddaf9f651ddb6f0f854a
terry <terry20070320@gmail.com>
2026-06-22 00:48:38 +0800

    檔案上傳

---
+++ op-assign1.py
@@ -0,0 +1,4 @@
+x = 14 ; y = 5 ; x+=y ; print (x)
+x = 14 ; y = 5 ; x-=y ; print (x)
+x = 14 ; y = 5 ; x*=y ; print (x)
+x = 14 ; y = 5 ; x/=y ; print (x)
---
+++ op-assign3.py
@@ -0,0 +1,4 @@
+x,y = 291,12.1
+print (x,y)
+x,y=y,x
+print (x,y)
